dd57e1554f9f2d5633076006b3378603e95f006c
madelinetharp <89538026+madelinetharp@users.noreply.github.com>
2023-06-12 11:22:32 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,14 @@
 var project = [
-    'Open-source', 'Affordable','Educational', 'Academic', 'Proprietary', 'Crowdfunded', '3D printed', 'Laser cut','Raspberry Pi', 'Arduino', 'Digital'
+    'Aperitivo', 'Big Deal','Al Pastor', 'Culvers', 'Grassroots', 'Benelux', 'Shake Shack', 'Oscars Winners Circle','Rocky Rococos', 'Swinging Door', 'Cubanitas', 'Potbelly', 'Cafe 111', 'Spicy Tuna', 'Public Market', 'Xankia', 'GoldenGyros', 'Palermo Pizza', 'Popeyes', 'Martinos', 'IHOP', '3rd Street', 'Panda Express'
 ]
 
-var extra = [
-    'ice cream truck', 'robot', 'autonomous vehicle', 'video game', 'card game', 'board game', 'augmented reality', 'plant watering system', 'doorstop', 'animatronic', 'security camera', 'motion sensor', 'pet feeder', 'fitness tracker', 'app'
-]
 
 function newProject() {
 var random = Math.floor(Math.random() * (project.length));
 document.getElementById('projectDisplay').innerHTML = project[random];
 }
 
-function newExtra() {
-    var random = Math.floor(Math.random() * (extra.length));
-    document.getElementById('extraDisplay').innerHTML = extra[random];
-    }
 
 function newFinal() {
         newProject();
-        newExtra();
     }
